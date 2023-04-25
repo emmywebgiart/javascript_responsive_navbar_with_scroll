0802c0913cfbc252f260c63e500d4fff820c063d
Emmanuel-Nowaii <eam@nowaii.com>
2023-04-24 18:43:00 -0600

    Feat: Separar estilos en archivos separados, agregar animaciones de menu hamburguer en lineas

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let menuButton = document.getElementById("menu_button");
+let menuNav = document.getElementById("menu_nav");
+const buttonsLine = document.querySelectorAll(".button_line");
+const classAnimations = ["rotate1", "scale2", "rotate3"];
+
+menuButton.onclick = () => {
+    menuNav.classList.toggle("active_nav");
+    buttonsLine.forEach((item, index) => {
+        item.classList.toggle(classAnimations[index])
+    });
+}
